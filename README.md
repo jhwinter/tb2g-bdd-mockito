e5c7fe051329b467af4168ff424b0e0d5d05c479
jhwinter <14960780+jhwinter@users.noreply.github.com>
2024-05-30 17:02:06 -0400

    added notes and completed Behavior Driven Mockito section

--- README.md
+++ README.md
@@ -15,3 +15,30 @@ For questions and help:
 * Or post in the Slack Community exclusive to the course.
 
 GitHub Issues will not be addressed.
+
+
+
+## Notes
+
+### Behavior Driven Development
+
+* Commonly abbreviated as BDD
+* BDD was established to help people learn TDD
+* BDD is largely a different way of looking at testing
+* BDD focuses on behavior vs "tests"
+* "Unit Tests" are referred to as specification - ie specifications of behaviors
+* Test method name should be sentences - ie saveValidID
+
+### Given When Then
+
+* BDD tests are often written in a given-when-then context
+* This approach often helps you organize your thoughts when writing the test
+* **Given** - Setup of the test
+* **When** - Action of the test - ie when method is called
+* **Then** - Verification of expected results
+
+### Mockito and BDD
+
+* Mockito has added BDD support in class BDDMockito
+* static method `given` allows you to configure mocks
+* static method `then` allows you to verify mock interactions
--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -11,6 +11,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,63 +26,82 @@ class SpecialitySDJpaServiceTest {
 
     @Test
     void testDeleteByObject() {
+        // given
         Speciality speciality = new Speciality();
 
+        // when
         service.delete(speciality);
 
-        verify(specialtyRepository).delete(any(Speciality.class));
+        // then
+        then(specialtyRepository).should().delete(any(Speciality.class));
     }
 
     @Test
     void findByIdTest() {
+        // given
         Speciality speciality = new Speciality();
+        given(specialtyRepository.findById(1L)).willReturn(Optional.of(speciality));
 
-        when(specialtyRepository.findById(1L)).thenReturn(Optional.of(speciality));
-
+        // when
         Speciality foundSpecialty = service.findById(1L);
 
+        // then
         assertThat(foundSpecialty).isNotNull();
-
-        verify(specialtyRepository).findById(anyLong());
-
+        then(specialtyRepository).should().findById(anyLong());
+        then(specialtyRepository).shouldHaveNoMoreInteractions();
     }
 
     @Test
     void deleteById() {
-        service.deleteById(1l);
-        service.deleteById(1l);
+        // given - none
 
-        verify(specialtyRepository, times(2)).deleteById(1l);
+        // when
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        // then
+        then(specialtyRepository).should(times(2)).deleteById(1L);
     }
 
     @Test
     void deleteByIdAtLeast() {
-        service.deleteById(1l);
-        service.deleteById(1l);
+        // given
 
-        verify(specialtyRepository, atLeastOnce()).deleteById(1l);
+        // when
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        // then
+        then(specialtyRepository).should(atLeastOnce()).deleteById(1L);
     }
 
     @Test
     void deleteByIdAtMost() {
-        service.deleteById(1l);
-        service.deleteById(1l);
+        // when
+        service.deleteById(1L);
+        service.deleteById(1L);
 
-        verify(specialtyRepository, atMost(5)).deleteById(1l);
+        // then
+        then(specialtyRepository).should(atMost(5)).deleteById(1L);
     }
 
     @Test
     void deleteByIdNever() {
-        service.deleteById(1l);
-        service.deleteById(1l);
+        // when
+        service.deleteById(1L);
+        service.deleteById(1L);
 
-        verify(specialtyRepository, atLeastOnce()).deleteById(1l);
-
-        verify(specialtyRepository, never()).deleteById(5L);
+        // then
+        then(specialtyRepository).should(atLeastOnce()).deleteById(1L);
+        then(specialtyRepository).should(never()).deleteById(5L);
     }
 
     @Test
     void testDelete() {
+        // when
         service.delete(new Speciality());
+
+        // then
+        then(specialtyRepository).should().delete(any());
     }
 }
